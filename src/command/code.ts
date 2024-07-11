f770c819461d7ad051a756f08e8d83bb97353615
Code pianist <77950104+1739616529@users.noreply.github.com>
2024-07-11 15:42:47 +0800

    feat: log useing model

--- src/command/code.ts
+++ src/command/code.ts
@@ -13,6 +13,7 @@ export class CodeComment extends CommandAdapth {
         const action_editor_info = get_active_editor_info()
         const temp_doc = await create_temp_file(action_editor_info)
         
+        logger.info(`Using OpenAI model ${config.custom_model || config.model}`)
         const ai = new CreateOpenAiChat(config)
         const promp = `You are a programming language commentator.\nYou need to help me add comments to #{sourceLanguage} code as much as possible to make it readable for beginners.\nDo not change the original code, just add as detailed comments as possible,\nbecause my purpose is only to understand and read. Please use my native language #{locale} as the commenting language.\nPlease do not reply with any text other than the code, and do not use markdown syntax.\nHere is the code you need to comment on:\n\n#{content}
         `.replace("#{sourceLanguage}", action_editor_info.language).replace("#{content}", action_editor_info.content).replace("#{locale}", env.language)
